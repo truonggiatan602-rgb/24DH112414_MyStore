74886f736b619b433233c6ee73844ed765fd5d1f
TD <TD@ADMIN-PC>
2025-11-11 11:51:25 +0700

    NGUJYÊN

--- Models/OrderDetail.cs
+++ Models/OrderDetail.cs
@@ -19,6 +19,7 @@ namespace _24DH112414_MyStore.Models
         public int OrderID { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
+        public Nullable<decimal> TotalPrice { get; set; }
     
         public virtual Order Order { get; set; }
         public virtual Product Product { get; set; }
