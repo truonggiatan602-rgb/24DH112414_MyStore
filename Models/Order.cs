c4dd10d3f73681bc325a17c7d889c9dd3be303e3
TD <TD@ADMIN-PC>
2025-11-11 11:17:26 +0700

    a

--- Models/Order.cs
+++ Models/Order.cs
@@ -25,7 +25,9 @@ namespace _24DH112414_MyStore.Models
         public System.DateTime OrderDate { get; set; }
         public decimal TotalAmount { get; set; }
         public string PaymentStatus { get; set; }
-        public string AddressDelivery { get; set; }
+        public string ShippingDelivery { get; set; }
+        public string DeliveryMethod { get; set; }
+        public string PaymentMethod { get; set; }
     
         public virtual Customer Customer { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
